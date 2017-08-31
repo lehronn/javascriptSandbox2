33901ef682d903cfffa4a8b2f7d4ddd1535bb5ad
Mateusz Stomski <lehronn@gmail.com>
2017-08-31 18:58:25 +0200

    9.3 module

--- script.js
+++ script.js
@@ -12,14 +12,22 @@
 //console.log(getTriangleArea(a,h));
 
 // zadanie 9.2
-var maleNames = ['Mateusz', 'Józef'];
-var femaleNames = ['Aleksandra', 'Agnieszka'];
+//var maleNames = ['Mateusz', 'Józef'];
+//var femaleNames = ['Aleksandra', 'Agnieszka'];
+//
+//var allNames = maleNames.concat(femaleNames);
+//newName = prompt('Podaj nowe imię do dodania!');
+//
+//if (allNames.indexOf(newName) === -1 ) {
+//  allNames.push(newName);
+//} else {
+//  console.log('Imię już istnieje w bazie szefie.');
+//}
 
-var allNames = maleNames.concat(femaleNames);
-newName = prompt('Podaj nowe imię do dodania!');
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur = 'triceratops';
 
-if (allNames.indexOf(newName) === -1 ) {
-  allNames.push(newName);
-} else {
-  console.log('Imię już istnieje w bazie szefie.');
-}
+var wordUppercased = dinosaur.toUpperCase();
+var wordReplacedInArray = text.replace( 'Velociraptor', wordUppercased );
+var halfText = ( wordReplacedInArray.length )/2;
+console.log( wordReplacedInArray.slice ( 0, halfText ) );
