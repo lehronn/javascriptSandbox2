a7c2857a5c53e3ddd7462a9c69df30dfb698e4d9
Mateusz Stomski <lehronn@gmail.com>
2017-08-31 18:29:57 +0200

    module 9.2

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-var a = prompt('Podaj A trójkąta.');
-var h = prompt('Podaj H trójkąta.');
+//var a = prompt('Podaj A trójkąta.');
+//var h = prompt('Podaj H trójkąta.');
+//
+//function getTriangleArea(a,h) {
+//  if (a <= 0 || h <= 0) {
+//    console.log('Nieprawidłowe dane, >0.');
+//    return NaN;
+//  }
+//  return (a*h)/2;
+//}
+//console.log(getTriangleArea(a,h));
 
-function getTriangleArea(a,h) {
-  if (a <= 0 || h <= 0) {
-    console.log('Nieprawidłowe dane, >0.');
-    return NaN;
-  }
-  return (a*h)/2;
+var maleNames = ['Mateusz', 'Józef'];
+var femaleNames = ['Aleksandra', 'Agnieszka'];
+
+var allNames = maleNames.concat(femaleNames);
+newName = prompt('Podaj nowe imię do dodania!');
+
+if (allNames.indexOf(newName) === -1 ) {
+  allNames.push(newName);
+} else {
+  console.log('Imię już istnieje w bazie szefie.');
 }
-console.log(getTriangleArea(a,h));
